11246c43c1184825651ec2d0e195da691cec20b3
Hyerim926 <82012938+Hyerim926@users.noreply.github.com>
2021-10-13 21:35:47 +0900

    Update MemberVO.java

--- miniproject/src/main/java/com/mall/model/MemberVO.java
+++ miniproject/src/main/java/com/mall/model/MemberVO.java
@@ -2,30 +2,36 @@ package com.mall.model;
 
 public class MemberVO {
 
-	// 회원 아이디
+	//회원 id
 	private String memberId;
-
-	// 회원 비밀번호
+	
+	//회원 비밀번호
 	private String memberPw;
-
-	// 회원 이름
+	
+	//회원 이름
 	private String memberName;
-
-	// 회원 이메일
+	
+	//회원 연락처
+	private String memberPhone;
+	
+	//회원 이메일
 	private String memberMail;
 
-	// 회원 생년월일
+	//회원 생년월일
 	private String memberBirth;
-
-	// 회원 기본주소
-	private String memberBasicAddr;
-
-	// 회원 상세주소
-	private String memberDetailAddr;
-
+	
+	//회원 우편번호
+	private String memberAddr1;
+	
+	//회원 주소
+	private String memberAddr2;
+	
+	//회원 상세주소
+	private String memberAddr3;
+	
 	// 관리자 구분(0:일반사용자, 1:관리자)
 	private int adminCk;
-
+	
 	public String getMemberId() {
 		return memberId;
 	}
@@ -50,6 +56,14 @@ public class MemberVO {
 		this.memberName = memberName;
 	}
 
+	public String getMemberPhone() {
+		return memberPhone;
+	}
+
+	public void setMemberPhone(String memberPhone) {
+		this.memberPhone = memberPhone;
+	}
+
 	public String getMemberMail() {
 		return memberMail;
 	}
@@ -66,20 +80,28 @@ public class MemberVO {
 		this.memberBirth = memberBirth;
 	}
 
-	public String getMemberBasicAddr() {
-		return memberBasicAddr;
+	public String getMemberAddr1() {
+		return memberAddr1;
 	}
 
-	public void setMemberBasicAddr(String memberBasicAddr) {
-		this.memberBasicAddr = memberBasicAddr;
+	public void setMemberAddr1(String memberAddr1) {
+		this.memberAddr1 = memberAddr1;
 	}
 
-	public String getMemberDetailAddr() {
-		return memberDetailAddr;
+	public String getMemberAddr2() {
+		return memberAddr2;
 	}
 
-	public void setMemberDetailAddr(String memberDetailAddr) {
-		this.memberDetailAddr = memberDetailAddr;
+	public void setMemberAddr2(String memberAddr2) {
+		this.memberAddr2 = memberAddr2;
+	}
+
+	public String getMemberAddr3() {
+		return memberAddr3;
+	}
+
+	public void setMemberAddr3(String memberAddr3) {
+		this.memberAddr3 = memberAddr3;
 	}
 
 	public int getAdminCk() {
@@ -92,11 +114,9 @@ public class MemberVO {
 
 	@Override
 	public String toString() {
-		return "MemberVO [memberId=" + memberId + ", memberPw=" + memberPw + ", memberName=" 
-						+ memberName + ", memberMail=" + memberMail + ", memberBirth=" + memberBirth 
-						+ ", memberBasicAddr=" + memberBasicAddr + ", memberDetailAddr=" + memberDetailAddr
-						+ ", adminCk=" + adminCk;
+		return "MemberVO [memberId=" + memberId + ", memberPw=" + memberPw + ", memberName=" + memberName + ", memberPhone=" + memberPhone
+				+ ", memberMail=" + memberMail + ", memberBirth=" + memberBirth + ", memberAddr1=" + memberAddr1 
+				+ ", memberAddr2=" + memberAddr2 + ", memberAddr3=" + memberAddr3 + ", adminCk=" + adminCk + "]";
 	}
-
 	
 }
